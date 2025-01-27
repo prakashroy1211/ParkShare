5822ac83b1495c1ff8912e58a8ce9156c0abba00
Dinasree P <dinasreep@gmail.com>
2025-01-27 23:01:06 +0530

    Sign in- images updated

--- parkshare/parkshare/settings.py
+++ parkshare/parkshare/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # This ensures Django knows where to look for static files.
+]
+
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
